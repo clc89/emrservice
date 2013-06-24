6b3c58b5734343e3e499a6cfa513a56a6342d1e9
Corey <clc89@zips.uakron.edu>
2013-06-24 16:30:22 -0400

    Added example client.

---
+++ EMRSampleClient/PhysicianExample.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PhysicianExample.aspx.cs" Inherits="EMRSampleClient.PhysicianExample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Physician Example</h1>
+    <form id="form1" runat="server">
+        <div style="position:absolute;top:80px;width:430px;border:solid;border-width:1px;padding:5px;">
+            <!-- Search -->
+            <div>
+                <asp:Label ID="Label12" runat="server" Text="Search by ID" Width="100px"></asp:Label>
+                <asp:TextBox ID="txtSearch" runat="server" Width="250px"></asp:TextBox>
+                <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+            </div>
+        </div>
+        <div style="border-style: solid; border-color: inherit; position:absolute; top:170px; width:400px; border-width:1px; padding:5px; left: 10px; height: 515px;">
+            <div style="position: relative; top: 10px; left: 5px;">
+                <!-- Id -->
+                <div>
+                    <asp:Label ID="Label13" runat="server" Text="ID" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtID" runat="server" Width="200px" Enabled="false" ForeColor="#999999"></asp:TextBox>
+                </div>
+            </div>
+
+             <div style="position: relative; top: 15px; left: 5px;">
+                 <h3>Name</h3>
+                <!-- Name -->
+                <div>
+                    <asp:Label ID="Label6" runat="server" Text="First" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtFirst" runat="server" Width="200px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label7" runat="server" Text="Last" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtLast" runat="server" Width="200px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label8" runat="server" Text="Middle" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtMiddle" runat="server" Width="200px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label9" runat="server" Text="Suffix" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtSuffix" runat="server" Width="50px"></asp:TextBox>
+                </div>
+            </div>
+            <div style="position: relative; top: 15px; left: 5px;">
+                 <h3>Address</h3>
+                <!-- Address -->
+                <div>
+                    <asp:Label ID="Label1" runat="server" Text="Street" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtStreet1" runat="server" Width="250px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label2" runat="server" Text="Street" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtStreet2" runat="server" Width="250px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label3" runat="server" Text="City" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtCity" runat="server" Width="250px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label4" runat="server" Text="State" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtState" runat="server" Width="75px"></asp:TextBox>
+                </div>
+                <div>
+                    <asp:Label ID="Label5" runat="server" Text="Zip" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtZip" runat="server" Width="75px"></asp:TextBox>
+                </div>
+            </div>
+
+            <div style="position: relative; top: 40px; left: 5px;">
+                <!-- Number -->
+                <div>
+                    <asp:Label ID="Label10" runat="server" Text="Phone #" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtNumber" runat="server" Width="200px"></asp:TextBox>
+                </div>
+            </div>
+            <div style="position: relative; top: 60px; left: 5px;">
+                <!-- Speciality -->
+                <div>
+                    <asp:Label ID="Label11" runat="server" Text="Speciality" Width="75px"></asp:Label>
+                    <asp:TextBox ID="txtSpeciality" runat="server" Width="250px"></asp:TextBox>
+                </div>
+            </div>
+            <div style="position: relative; top: 90px; left: 1px;">
+                <asp:Button ID="btnCreate" runat="server" Text="Create" OnClick="btnCreate_Click" />
+                <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+            </div>
+        </div>
+
+    </form>
+
+    
+</body>
+</html>
---
+++ EMRSampleClient/PhysicianExample.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EMRServiceModels.Models;
+using RestSharp;
+
+namespace EMRSampleClient
+{
+    public partial class PhysicianExample : System.Web.UI.Page
+    {
+        // RestSharp library object uses service url string from web.config
+        RestClient client = new RestClient(ConfigurationManager.AppSettings["ServiceURL"].ToString());
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            ClearData();
+
+            #region Request Physician using RestSharp library
+
+            RestRequest request = new RestRequest("physician/{id}", Method.GET);
+            request.AddUrlSegment("id", txtSearch.Text);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("key", "f81d4fae-7dec-11d0-a765-00a0c91e6bf6");
+            IRestResponse<Physician> response = client.Execute<Physician>(request);
+
+            #endregion
+
+            #region Fill in UI objects
+            if (response.Data != null)
+            {
+                txtID.Text = response.Data.Id;
+
+                if (response.Data.PhysicianAddress != null)
+                {
+                    txtStreet1.Text = response.Data.PhysicianAddress.Street;
+                    txtStreet2.Text = response.Data.PhysicianAddress.Street2;
+                    txtCity.Text = response.Data.PhysicianAddress.City;
+                    txtState.Text = response.Data.PhysicianAddress.State;
+                    txtZip.Text = response.Data.PhysicianAddress.ZipCode;
+                }
+
+                if (response.Data.PhysicianName != null)
+                {
+                    txtFirst.Text = response.Data.PhysicianName.FirstName;
+                    txtLast.Text = response.Data.PhysicianName.LastName;
+                    txtMiddle.Text = response.Data.PhysicianName.MiddleInitial;
+                    txtSuffix.Text = response.Data.PhysicianName.Suffix;
+                }
+
+                txtNumber.Text = response.Data.PhoneNumber;
+
+                txtSpeciality.Text = response.Data.Speciality;
+            }
+            #endregion
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+
+            #region Create Physician object based on UI values
+
+            Physician physician = new Physician();
+            Address address = new Address();
+            Name name = new Name();
+
+            address.Street = txtStreet1.Text;
+            address.Street2 = txtStreet2.Text;
+            address.City = txtCity.Text;
+            address.State = txtState.Text;
+            address.ZipCode = txtZip.Text;
+            physician.PhysicianAddress = address;
+
+            name.FirstName = txtFirst.Text;
+            name.LastName = txtLast.Text;
+            name.MiddleInitial = txtMiddle.Text;
+            name.Suffix = txtSuffix.Text;
+            physician.PhysicianName = name;
+
+            physician.PhoneNumber = txtNumber.Text;
+
+            physician.Speciality = txtSpeciality.Text;
+
+            #endregion
+
+            #region Post Physician object using RestSharp library
+
+            RestRequest request = new RestRequest("physician", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(physician);           
+            request.AddHeader("key", "f81d4fae-7dec-11d0-a765-00a0c91e6bf6");
+            IRestResponse<Physician> response = client.Execute<Physician>(request);
+
+            #endregion
+
+            // Display ID created by service for Physician
+            if (response.Data != null)
+            {
+                txtID.Text = response.Data.Id;
+            }
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            #region Create Physician object based on UI values
+
+            Physician physician = new Physician();
+            Address address = new Address();
+            Name name = new Name();
+
+            physician.Id = txtID.Text;
+            address.Street = txtStreet1.Text;
+            address.Street2 = txtStreet2.Text;
+            address.City = txtCity.Text;
+            address.State = txtState.Text;
+            address.ZipCode = txtZip.Text;
+            physician.PhysicianAddress = address;
+
+            name.FirstName = txtFirst.Text;
+            name.LastName = txtLast.Text;
+            name.MiddleInitial = txtMiddle.Text;
+            name.Suffix = txtSuffix.Text;
+            physician.PhysicianName = name;
+
+            physician.PhoneNumber = txtNumber.Text;
+
+            physician.Speciality = txtSpeciality.Text;
+
+            #endregion
+
+            #region Put Physician object using RestSharp library
+
+            RestRequest request = new RestRequest("physician", Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(physician);
+            request.AddHeader("key", "f81d4fae-7dec-11d0-a765-00a0c91e6bf6");
+            IRestResponse<Physician> response = client.Execute<Physician>(request);
+
+            #endregion
+        }
+
+        // helper method for clearing UI
+        private void ClearData()
+        {
+            txtID.Text = string.Empty;
+            txtStreet1.Text = string.Empty;
+            txtStreet2.Text = string.Empty;
+            txtCity.Text = string.Empty;
+            txtState.Text = string.Empty;
+            txtZip.Text = string.Empty;
+            txtFirst.Text = string.Empty;
+            txtLast.Text = string.Empty;
+            txtMiddle.Text = string.Empty;
+            txtSuffix.Text = string.Empty;
+            txtNumber.Text = string.Empty;
+            txtSpeciality.Text = string.Empty;
+        }
+
+    }
+}
